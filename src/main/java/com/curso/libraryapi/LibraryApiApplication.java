ed700058d6bbc338940646a2e829449ba2b62cea
ruthgabrielle <ruthfgms@gmail.com>
2021-02-11 16:00:24 -0300

    Adicionando o ModelMapper ao projeto

--- src/main/java/com/curso/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/curso/libraryapi/LibraryApiApplication.java
@@ -1,10 +1,18 @@
 package com.curso.libraryapi;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ui.ModelMap;
 
 @SpringBootApplication
 public class LibraryApiApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryApiApplication.class, args);
